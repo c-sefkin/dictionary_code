c83c057b752e3606d5e7e4588b5f8a4206c467e5
Chris Sefkin <c.sefkin@gmail.com>
2015-05-01 10:39:11 -0700

    return the name of a word, empty out all of the saved words, and add a word to the array of saved words

--- spec/definition_integration_spec.rb
+++
@@ -1,8 +0,0 @@
-require('capybara/rspec')
-require('./app')
-Capybara.app = Sinatra::Application
-
-describe('the definition path', {:type => :feature}) do
-  it('processes the user entry . ..') do
-    visit('/')
-  
