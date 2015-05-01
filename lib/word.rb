3fa00db23d0b0cd131028ba94aa5b9e39d282f4a
Chris Sefkin <c.sefkin@gmail.com>
2015-05-01 10:49:02 -0700

    return a word by it's id number

--- lib/word.rb
+++ lib/word.rb
@@ -31,4 +31,15 @@ class Word
     @defs
   end
 
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+
 end
